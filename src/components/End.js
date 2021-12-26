1ddb7ea794fc22eb304094f280f7c61ea9947da1
Adarsh <adarshgupta2468@gmail.com>
2021-12-26 20:48:04 +0530

    mages studio website update

--- src/components/End.js
+++ src/components/End.js
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 export default function End(){
     return(
-        <div style={{fontFamily:"Poppins"}}>
+        <div style={{fontFamily:"Poppins"}} className="mb-4">
 
             <div className=" w-4/5 m-auto hidden md:block bg-gray-200 " style={{height:"2px"}}/>
             <div className="flex items-center justify-around">
-           <Link to="/"> <p  className="text-xl md:text-2xl pt-8  text-gray-400 hover:text-blue-400">Mages</p></Link>
+           <Link to="/"> <p  className="text-xl md:text-2xl pt-8  text-gray-400 hover:text-blue-400">Mages Studio Pvt Ltd</p></Link>
 <span className="md:flex hidden items-center pt-8  text-gray-400 justify-between" style={{fontFamily:"Poppins"}}>
             <p className="mx-2 hover:text-blue-400">Cookies </p>
             <p className="mx-2 hover:text-blue-400">   Privacy and Policies</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-      extend: {},
+      extend: {
+        colors:{blue:{460:"#44b2e5"}}
+      },
     },
     variants: {
       extend: {},
